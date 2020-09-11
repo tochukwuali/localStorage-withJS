3d91da431ec044004a62d49834ad5abc573097c0
tcee42 <toochukwuali@gmail.com>
2020-09-11 09:11:09 +0100

    learning localStorage in JS

---
+++ index.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('form')
+const clearButton = document.getElementById('clear')
+const ul = document.querySelector('ul')
+const input = document.getElementById('item')
+
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []
+
+localStorage.setItem('items', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('items'))
+
+const makeLIistItems = (text) => {
+    const li = document.createElement('li')
+    li.textContent = text
+    ul.appendChild(li)
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    let textInput = input.value
+    
+    itemsArray.push(textInput);
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+    makeLIistItems(textInput)
+    input.value = ''
+})
+
+data.map((item) => {
+    makeLIistItems(item)
+})
+
+clearButton.addEventListener('click', function() {
+    localStorage.clear()
+    if (ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+    }
+})
